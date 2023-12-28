5a152169d6e0f22a6c0c6bf1495f1593ada69350
Pukar Sharma <sharma.pukar99@gmail.com>
2023-12-28 17:18:40 +0545

    question

--- Q19.java
+++ Q19.java
@@ -1,3 +1,3 @@
 public class Q19 {
     
-}
+}//// Q.2 WAP to create table named Student.
---
+++ Q20.java
@@ -0,0 +1,4 @@
+public class Q20 {
+    
+}
+//// Q.3 WAP to perform CRUD operation to table Student. (4 Different Programs)
